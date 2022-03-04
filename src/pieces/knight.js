cacc9d0084c754d91da35533bcc804cd351ceb0f
jscicchitano2 <jscicchitano2@gmail.com>
2022-03-03 20:12:30 -0800

    check if knight move possible

--- src/pieces/knight.js
+++ src/pieces/knight.js
@@ -1,4 +1,5 @@
 import Piece from './piece.js';
+import { isSameRow } from '../helpers';
 
 export default class Knight extends Piece {
   constructor(player) {
@@ -6,5 +7,24 @@ export default class Knight extends Piece {
         "https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg" : 
         "https://upload.wikimedia.org/wikipedia/commons/e/ef/Chess_ndt45.svg")
     );
+  }
+
+  isMovePossible(src, dest) {
+    return ((src - 17 === dest && !isSameRow(src, dest)) ||
+      (src - 10 === dest && !isSameRow(src, dest)) ||
+      (src + 6 === dest && !isSameRow(src, dest)) ||
+      (src + 15 === dest && !isSameRow(src, dest)) ||
+      (src - 15 === dest && !isSameRow(src, dest)) ||
+      (src - 6 === dest && !isSameRow(src, dest)) ||
+      (src + 10 === dest && !isSameRow(src, dest)) ||
+      (src + 17 === dest && !isSameRow(src, dest)))
+  }
+
+  /**
+   * always returns empty array because of jumping
+   * @return {[]}
+   */
+   getSrcToDestPath() {
+    return [];
   }
 }
